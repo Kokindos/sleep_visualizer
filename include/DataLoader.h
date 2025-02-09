dffbc69f5c897418e7b5d32ab3df7510be716a75
kokindos <kokindoss@gmail.com>
2025-02-10 02:01:09 +0300

    store time in correct types

--- include/DataLoader.h
+++ include/DataLoader.h
@@ -6,6 +6,7 @@
 #include <vector>
 #include <chrono>
 
+using DateTime = std::chrono::system_clock::time_point;
 
 enum class SleepPhaseType {
     Light,
@@ -14,20 +15,18 @@ enum class SleepPhaseType {
     Awake
 };
 
-SleepPhaseType fromString(const std::string &phaseStr);
-
 // фаза сна
 struct SleepPhase {
     SleepPhaseType type;
-    std::string start;
-    std::string end;
+    DateTime start;
+    DateTime end;
 };
 
 // сон за один день
 struct DailySleepData {
-    std::string date;
-    std::string bedtime;
-    std::string wakeTime;
+    DateTime date;
+    DateTime bedtime;
+    DateTime wakeTime;
     std::vector<SleepPhase> phases;
 //    для будущих визуализаций
 //    int averageHeartRate;
@@ -37,12 +36,15 @@ struct DailySleepData {
 
 class DataLoader {
 public:
-
-    static DailySleepData LoadFromJsonFile(const std::string &filename);
+    static DailySleepData loadFromJsonFile(const std::string &filename);
 
     //todo лучше перейти на один файл
-    static std::vector<DailySleepData> LoadFromMultipleJsonFiles(const std::vector<std::string> &filenames);
-};
+    static std::vector<DailySleepData> loadFromMultipleJsonFiles(const std::vector<std::string> &filenames);
 
+private:
+    static SleepPhaseType fromString(const std::string &phaseStr);
+
+    static DateTime parseDateTime(const std::string &dateTimeStr, const std::string &format = "%Y-%m-%d %H:%M");
+};
 
 #endif //SLEEP_VISUALIZER_DATALOADER_H
--- src/DataLoader.cpp
+++ src/DataLoader.cpp
@@ -2,17 +2,11 @@
 #include <fstream>
 #include <nlohmann/json.hpp>
 #include <iostream>
+#include <sstream>
 
-SleepPhaseType fromString(const std::string &phaseStr) {
-    if (phaseStr == "Light") return SleepPhaseType::Light;
-    if (phaseStr == "Deep") return SleepPhaseType::Deep;
-    if (phaseStr == "REM") return SleepPhaseType::REM;
-    if (phaseStr == "Awake") return SleepPhaseType::Awake;
-    throw std::invalid_argument("Invalid sleep phase string");
-}
-
-DailySleepData DataLoader::LoadFromJsonFile(const std::string &filename) {
+DailySleepData DataLoader::loadFromJsonFile(const std::string &filename) {
     DailySleepData result;
+
     try {
         std::ifstream ifs(filename);
         if (!ifs.is_open()) {
@@ -23,17 +17,24 @@ DailySleepData DataLoader::LoadFromJsonFile(const std::string &filename) {
         nlohmann::json j;
         ifs >> j;
 
-        result.date = j.value("date", "");
-        result.bedtime = j.value("bedtime", "");
-        result.wakeTime = j.value("wake_time", "");
+        std::string dateStr = j.value("date", "");
+        std::string bedtimeStr = j.value("bedtime", "");
+        std::string wakeTimeStr = j.value("wake_time", "");
+
+        result.date = parseDateTime(dateStr, "%Y-%m-%d");
+        //todo указывать в json'e дату
+        result.bedtime = parseDateTime(dateStr + " " + bedtimeStr);
+        result.wakeTime = parseDateTime(dateStr + " " + wakeTimeStr);
 
         if (j.contains("phases") && j["phases"].is_array()) {
             for (auto &phase: j["phases"]) {
+                //todo try?
                 SleepPhase sp;
 
+                //todo заменить на at?
                 sp.type = fromString(phase.value("type", ""));
-                sp.start = phase.value("start", "");
-                sp.end = phase.value("end", "");
+                sp.start = parseDateTime(phase.value("start", ""));
+                sp.end = parseDateTime(phase.value("end", ""));
                 result.phases.push_back(sp);
             }
         }
@@ -43,12 +44,12 @@ DailySleepData DataLoader::LoadFromJsonFile(const std::string &filename) {
     return result;
 }
 
-std::vector<DailySleepData> DataLoader::LoadFromMultipleJsonFiles(const std::vector<std::string> &filenames) {
+std::vector<DailySleepData> DataLoader::loadFromMultipleJsonFiles(const std::vector<std::string> &filenames) {
     std::vector<DailySleepData> allData;
     for (const auto &file: filenames) {
 
         try {
-            DailySleepData data = LoadFromJsonFile(file);
+            DailySleepData data = loadFromJsonFile(file);
             allData.push_back(data);
         }
         catch (const std::exception &e) {
@@ -56,4 +57,23 @@ std::vector<DailySleepData> DataLoader::LoadFromMultipleJsonFiles(const std::vec
         }
     }
     return allData;
+}
+
+SleepPhaseType DataLoader::fromString(const std::string &phaseStr) {
+    if (phaseStr == "Light") return SleepPhaseType::Light;
+    if (phaseStr == "Deep") return SleepPhaseType::Deep;
+    if (phaseStr == "REM") return SleepPhaseType::REM;
+    if (phaseStr == "Awake") return SleepPhaseType::Awake;
+    throw std::invalid_argument("invalid sleep phase string");
+}
+
+DateTime DataLoader::parseDateTime(const std::string &dateTimeStr, const std::string &format) {
+    std::tm tm = {};
+    std::istringstream ss(dateTimeStr);
+    ss >> std::get_time(&tm, format.c_str());
+
+    if (ss.fail()) {
+        throw std::invalid_argument("invalid date format: " + dateTimeStr);
+    }
+    return std::chrono::system_clock::from_time_t(std::mktime(&tm));
 }
