55f0b0d4b4d3d1cbb99aadda9b3d5ab3749fca3c
kokindos <kokindoss@gmail.com>
2025-03-05 21:51:23 +0300

    start write docs

--- include/DateUtils.h
+++ include/DateUtils.h
@@ -4,6 +4,13 @@
 #include <chrono>
 #include <string>
 
+/**
+ * @brief Утилиты для обработки и форматирования даты и времени.
+ *
+ * Этот класс предоставляет статические методы для конвертации между разными представлениями времени
+ * и форматирования.
+ * Объекты этого класса создавать нельзя.
+ */
 class DateUtils {
 public:
     DateUtils() = delete;
@@ -12,18 +19,56 @@ public:
 
     DateUtils &operator=(const DateUtils &) = delete;
 
+    /**
+    * @brief Форматирует время в строку, содердащую только часы и минуты
+    *
+    * @param tp Временная точка, которую нужно отформатировать.
+    * @return Строка, представляющая время в формате ЧЧ:ММ.
+    */
     //todo возможно возвращать c_str()
     static std::string onlyTime(const std::chrono::system_clock::time_point &tp);
 
+    /**
+     * @brief Форматирует время в строку только с датой.
+     *
+     * @param tp Временная точка, которую нужно отформатировать.
+     * @return Строка с представленной датой в формате (YYYY-MM-DD).
+     */
     static std::string onlyDate(const std::chrono::system_clock::time_point &tp);
 
+    /**
+     * @brief Конвертирует Unix timestamp в time_point.
+     *
+     * @param unixTime Unix-время, представленное в виде числа с плавающей точкой.
+     * @return Соответствующий time_point объект.
+     */
     static std::chrono::system_clock::time_point unixToTimePoint(const double &unixTime);
 
+    /**
+     * @brief Конвертирует объект time_point в Unix timestamp.
+     *
+     * @param tp Временная точка
+     * @return Unix-время в виде числа с плавающей точкой.
+     */
     static double timePointToUnix(const std::chrono::system_clock::time_point &tp);
 
+    /**
+    * @brief Возвращает разницу в минутах между двумя временными точками.
+    *
+    * @param start Начальная временная точка.
+    * @param end Конечная временная точка.
+    * @return Разница между @p end и @p start в минутах.
+    */
     static int diffBetween(const std::chrono::system_clock::time_point &start,
                                const std::chrono::system_clock::time_point &end);
 
+
+    /**
+    * @brief Форматирует целочисленную разницу во времени в минутах в удобочитаемую строку.
+    *
+    * @param timeDiff Разница во времени в минутах.
+    * @return Стнрока вида X ч. Y мин.
+    */
     static std::string formatTimeDiff(const int& timeDiff);
 };
 
--- include/Visualization.h
+++ include/Visualization.h
@@ -6,12 +6,25 @@
 #include "DataLoader.h"
 #include "SleepAnalyzer.h"
 
+/**
+* @brief Класс, строящий графики ImPlot
+*/
 class Visualization {
 public:
+    /**
+    * @brief Отрисовывает график, визуализирующий таймлан фаз сна за день
+    *
+    * @param data - DailySleepData - данные о сне за сутки
+    */
     static void ShowDailyPhasesPlot(const DailySleepData &data);
 
-    static void ShowMetricsSummary(const SleepMetrics &m, const bool isAverage);
-
+    /**
+    * @brief Отрисовывает таблицы и графики, отражающие метрики сна за день или неделю
+    *
+    * @param m - SleepMetrics - посчитанные метрики сна
+    * @param isAverage - вид графика: за сутки(false) или за неделю (true)
+    */
+    static void ShowMetricsSummary(const SleepMetrics &m, bool isAverage);
 };
 
 #endif //SLEEP_VISUALIZER_VISUALIZATION_H
